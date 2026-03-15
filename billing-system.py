b81a0c258b3aa794193b8be8753d3b1e7b992b04
RohithReddyGaja <rohithreddygaja259@gmail.com>
2026-03-15 14:52:46 +0530

    Upgraded shopping cart with OOP and menu system

--- billing-system.py
+++ billing-system.py
@@ -1,20 +1,85 @@
-items = []
-prices = []
-total = 0
+class Shoping_cart:
 
-while True:
-    item = input("enter the items(q to quit): ")
-    if item.lower()== "q" :
-        break
-    else:
-        price = int(input(f"enter the price of {item}: $"))
-        items.append(item)
-        prices.append(price)
-print("-------------your cart----------------")
-for i in range(len(items)):
-    print(f"{items[i]} - ${prices[i]}")
+    def __init__(self):
+        self.cart = {}
 
-for price in prices:
-    total += price
-print("--------------------------------------")
-print(f"your total will be ${total} ")
+    def add_item(self):
+        item = input("Enter Item name:")
+
+        try:
+            price = float(input("Enter price : "))
+            quantity = int(input("Enter Quantity : "))
+        except ValueError:
+            print("Enter a valid input ")
+            return
+        
+        if item in self.cart:
+            self.cart[item]["qty"] += quantity
+
+        else:
+            self.cart[item] = {"price" : price, "qty" : quantity}
+
+        print(f"{item} added to cart")
+
+    def remove_item(self):
+        item = input("Enter item to remove : ")
+
+        if item in self.cart:
+            del self.cart[item]
+            print("Item removed")
+        else:
+            print("Item not found")
+            print("No items removed") 
+
+    def view_cart(self):
+        if not self.cart:
+            print("Cart is empty")
+            return
+        
+        print("\n------ YOUR CART ------")
+
+        total = 0
+
+        for item,data in self.cart.items():
+            subtotal = data["price"] * data["qty"]
+            total += subtotal
+            print(f"{item} ({data["qty"]}) - ${subtotal:.2f} ")
+
+        tax = total * 0.05
+        final_total = total + tax
+
+        
+        print("------------------------")
+        print(f"Subtotal: ${total:.2f}")
+        print(f"Tax (5%): ${tax:.2f}")
+        print(f"Total: ${final_total:.2f}")
+
+
+    def run(self):
+        while True:
+
+            print("\n1. Add Item")
+            print("2. Remove Item")
+            print("3. View Cart")
+            print("4. Exit")
+
+            choice = input("Select option : ")
+
+            if choice == "1":
+                self.add_item()
+
+            elif choice == "2":
+                self.remove_item()
+
+            elif choice == "3":
+                self.view_cart()
+
+            elif choice == "4":
+                print("Thank you for shopping.")
+                break
+
+            else:
+                print("Enter a valid option ")
+
+cart = Shoping_cart()
+cart.run()
